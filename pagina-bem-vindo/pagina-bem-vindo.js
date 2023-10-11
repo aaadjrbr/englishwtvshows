5791f5b96eee694b1379feef7942a85082fa379e
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-10-11 19:07:02 -0300

    updating-hallowen-page-welcome

--- pagina-bem-vindo/pagina-bem-vindo.js
+++ pagina-bem-vindo/pagina-bem-vindo.js
@@ -1,10 +1,15 @@
-    // Set the time delay in milliseconds (5 seconds in this example)
-    const timeDelay = 2000;
+// Set the time delay in milliseconds (2 seconds in this example)
+const timeDelay = 2000;
 
-    // Function to perform the redirection
-    function redirect() {
-      window.location.href = "./index-second.html"; // Replace with your desired URL
-    }
+// Function to perform the redirection
+function redirect() {
+  window.location.href = "./index-second.html"; // Replace with your desired URL
+}
 
-    // Set a timer to call the redirect function after the specified time delay
-    setTimeout(redirect, timeDelay);
+// Set a timer to call the redirect function after the specified time delay
+setTimeout(redirect, timeDelay);
+
+// Add an event listener to trigger the redirection when leaving the page
+window.onbeforeunload = function () {
+  redirect();
+};
