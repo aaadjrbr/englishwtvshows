9ecd143e2aaaf3ac701f9e35dd89d1fdde55b2df
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-10-26 18:05:28 -0300

    fixing-and-uptading-music-section

--- teste-grauito/pearl-jam-gratis/pearl-jam-gratis.js
+++ teste-grauito/pearl-jam-gratis/pearl-jam-gratis.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       partLabel: "Parte 1",
       phrases: [
-        "Oh where, o where, can my ....",
+        "Oh where, o where, can my ...",
         "The Lord took her away from ...",
         "She's gone to ...",
         "So I've got to be ...",
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
         ["world"],
         // Add four more sets of answers here for the first part
       ],
-      driveFileId: "1Cw0XZHlMi0oqyHXVS8dEXxN7UX6oW8dp" // Replace with your Google Drive file ID for the first part
+      audioURL: "https://firebasestorage.googleapis.com/v0/b/english-with-tv-shows.appspot.com/o/music-section%2Fpearl-jam%2Flast-kiss-audios%2FLast-kiss-part1.mp3?alt=media&token=3137d7a6-3095-4294-909f-dfa591c84446&_gl=1*khvvl6*_ga*MTc3Mzk3ODA2Ni4xNjk0NzIzNTU1*_ga_CW55HF8NVT*MTY5ODMzMDI0OS4xMy4xLjE2OTgzNDE3NjEuMzcuMC4w" // Replace with your Google FireBase link for each part
     },
     {
       partLabel: "Parte 2",
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
           ["stalled"],
           ["dead"],
       ],
-      driveFileId: "17Z3486If9dsitFA2D-Rr257Cfg-eU0em" // Replace with your Google Drive file ID for the second part
+      audioURL: "https://firebasestorage.googleapis.com/v0/b/english-with-tv-shows.appspot.com/o/music-section%2Fpearl-jam%2Flast-kiss-audios%2FLast-kiss-part2.mp3?alt=media&token=3456c0a3-7398-4112-9a2c-04a59b1e6f85&_gl=1*1qs55ii*_ga*MTc3Mzk3ODA2Ni4xNjk0NzIzNTU1*_ga_CW55HF8NVT*MTY5ODMzMDI0OS4xMy4xLjE2OTgzNDI0NDMuNjAuMC4w" // Replace with your Google FireBase link for each part
     },
     {
       partLabel: "Parte 3",
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded", function () {
         ["glass"],
         ["last"],
       ],
-      driveFileId: "1VEOE_Fio7GvaS3k9KiDkoEF2XkXLv-9E" // Replace with your Google Drive file ID for the second part
+      audioURL: "https://firebasestorage.googleapis.com/v0/b/english-with-tv-shows.appspot.com/o/music-section%2Fpearl-jam%2Flast-kiss-audios%2FLast-kiss-part3.mp3?alt=media&token=f813a50d-8ea9-4ea7-93e8-4be282a35891&_gl=1*dpy8pt*_ga*MTc3Mzk3ODA2Ni4xNjk0NzIzNTU1*_ga_CW55HF8NVT*MTY5ODMzMDI0OS4xMy4xLjE2OTgzNDI0OTYuNy4wLjA." // Replace with your Google FireBase link for each part
     },
     {
       partLabel: "Parte 4",
@@ -114,7 +114,7 @@ document.addEventListener("DOMContentLoaded", function () {
         ["eyes"],
         ["night"],
       ],
-      driveFileId: "1EWFqEI3pU63NfxyR5kJOuIh4GuKkKOR4" // Replace with your Google Drive file ID for the second part
+      audioURL: "https://firebasestorage.googleapis.com/v0/b/english-with-tv-shows.appspot.com/o/music-section%2Fpearl-jam%2Flast-kiss-audios%2FLast-kiss-part4.mp3?alt=media&token=e1e06bc0-851a-4fd6-be1f-f9064cc5db47&_gl=1*mkv5b8*_ga*MTc3Mzk3ODA2Ni4xNjk0NzIzNTU1*_ga_CW55HF8NVT*MTY5ODMzMDI0OS4xMy4xLjE2OTgzNDI1MzEuNjAuMC4w" // Replace with your Google FireBase link for each part
     },
     // Add more parts with labels, phrases, options, and answers here
   ];
@@ -135,181 +135,197 @@ document.addEventListener("DOMContentLoaded", function () {
   const pauseButton = document.getElementById("pause-button");
   const restartAudioButton = document.getElementById("restart-audio-button");
   const audio = document.getElementById("audio");
+  const fastForwardImage = document.getElementById("fast-forward-button");
+  const rewindImage = document.getElementById("rewind-button");
 
 
-  // Add event listeners for the audio control buttons
+  // Forward & Rewind Buttons
+  fastForwardImage.addEventListener("click", fastForward);
+  rewindImage.addEventListener("click", rewind);
 
-  playButton.addEventListener("click", () => {
-    if (currentAudio) {
-      currentAudio.play();
-    }
-  });
-
-  pauseButton.addEventListener("click", () => {
-    if (currentAudio) {
-      currentAudio.pause();
-    }
-  });
-
-  restartAudioButton.addEventListener("click", () => {
-    if (currentAudio) {
-      currentAudio.currentTime = 0;
-      currentAudio.play();
-    }
-  });
-
-
-  // Add an event listener to the restart button
-restartButton.addEventListener("click", () => {
-  // Reset the current song and phrase index, as well as the score
-  currentSongIndex = 0;
-  currentPhraseIndex = 0;
-  score = 0;
-  
-  // Load the first phrase of the first part
-  loadPhrase(currentSongIndex, currentPhraseIndex);
-  
-  // Show the skip button
-  skipButton.style.display = "block";
-});
-
-// Add an event listener to the skip button
-skipButton.addEventListener("click", () => {
-  // Check if there are more parts to skip to
-  if (currentSongIndex < songsData.length - 1) {
-    // Pause the current audio before loading the new part
-    if (currentAudio) {
-      currentAudio.pause();
-    }
-
-    // Increase the current song index to skip to the next part
-    currentSongIndex++;
-    currentPhraseIndex = 0; // Reset the phrase index for the new part
-    score = 0; // Reset the score for the new part
-    // Load the first phrase of the new part
-    loadPhrase(currentSongIndex, currentPhraseIndex);
+// Function to fast forward 3 seconds
+function fastForward() {
+  if (currentAudio) {
+    currentAudio.currentTime += 3; // Adjust the time you want to fast forward by
   }
-});
-
-    // Add an event listener to the go back button
-goBackButton.addEventListener("click", () => {
-  // Check if there are previous parts to go back to
-  if (currentSongIndex > 0) {
-    // Decrease the current song index to go back to the previous part
-    currentSongIndex--;
-    currentPhraseIndex = 0; // Reset the phrase index for the previous part
-    score = 0; // Reset the score for the previous part
-    // Load the first phrase of the previous part
-    loadPhrase(currentSongIndex, currentPhraseIndex);
-    
-    // Show the skip button again
-    skipButton.style.display = "block";
-  }
-});
-
-function loadAudio(driveFileId) {
-  const driveFileURL = `https://drive.google.com/uc?id=${driveFileId}`;
-  currentAudio = new Audio(driveFileURL);
-  audio.src = "";
-  audio.load();
 }
 
-  function loadPhrase(songIndex, phraseIndex) {
-      const currentSong = songsData[songIndex];
-      const phrase = currentSong.phrases[phraseIndex];
-      const options = currentSong.options[phraseIndex];
-      const audioId = `audio-${songIndex}-${phraseIndex}`; // Unique audio element ID
+// Function to rewind 3 seconds
+function rewind() {
+  if (currentAudio) {
+    currentAudio.currentTime -= 3; // Adjust the time you want to rewind by
+  }
+}
+  // Function to load audio
+  function loadAudio(audioUrl) {
+    if (currentAudio) {
+      currentAudio.pause();
+    }
+    currentAudio = new Audio(audioUrl);
+    audio.src = audioUrl;
+    audio.load();
     
-      // Store the current playback position
-      const currentTime = audio.currentTime;
-    
-      phraseContainer.textContent = phrase;
-      partLabelContainer.textContent = currentSong.partLabel;
-      optionsContainer.innerHTML = "";
-    
-      options.forEach((option, index) => {
-        const optionBox = document.createElement("div");
-        optionBox.classList.add("option-box");
-        optionBox.textContent = option;
-        optionBox.addEventListener("click", () => {
-          selectOption(option, songIndex, phraseIndex);
-        });
-    
-        optionsContainer.appendChild(optionBox);
-      });
-    
-      loadAudio(currentSong.driveFileId, audioId);
-      scoreDisplay.textContent = `Frases Concluídas: ${score}`;
-    
-      // Set the playback position to the stored value
-      audio.currentTime = currentTime;
-    }      
-    
+    // Reattach event listeners for audio control buttons
+    playButton.addEventListener("click", () => {
+      if (currentAudio) {
+        currentAudio.play();
+      }
+    });
 
-  function selectOption(selectedOption, songIndex, phraseIndex) {
-      const currentSong = songsData[songIndex];
-      const currentAnswers = currentSong.answers[phraseIndex];
-    
-      const optionBoxes = optionsContainer.querySelectorAll(".option-box");
-    
-      // Remove the 'correct' and 'wrong' classes from all option boxes
-      optionBoxes.forEach((box) => {
-        box.classList.remove("correct", "wrong", "inactive");
+    pauseButton.addEventListener("click", () => {
+      if (currentAudio) {
+        currentAudio.pause();
+      }
+    });
+
+    restartAudioButton.addEventListener("click", () => {
+      if (currentAudio) {
+        currentAudio.currentTime = 0;
+        currentAudio.play();
+      }
+    });
+  }
+
+  // Function to load a phrase
+  function loadPhrase(songIndex, phraseIndex) {
+    const currentSong = songsData[songIndex];
+    const phrase = currentSong.phrases[phraseIndex];
+    const options = currentSong.options[phraseIndex];
+
+    phraseContainer.textContent = phrase;
+    partLabelContainer.textContent = currentSong.partLabel;
+    optionsContainer.innerHTML = "";
+
+    options.forEach((option, index) => {
+      const optionBox = document.createElement("div");
+      optionBox.classList.add("option-box");
+      optionBox.textContent = option;
+      optionBox.addEventListener("click", () => {
+        selectOption(option, songIndex, phraseIndex);
       });
-    
-      if (currentAnswers.includes(selectedOption)) {
-        optionBoxes.forEach((box) => {
-          if (box.textContent === selectedOption) {
-            box.classList.add("correct");
-          } else {
-            box.classList.add("inactive");
-          }
-        });
-        phraseContainer.textContent = "✔️ Correto!";
-        score++;
-        setTimeout(() => {
-          nextPhrase(songIndex, phraseIndex);
-        }, 1000);
+
+      optionsContainer.appendChild(optionBox);
+    });
+
+    loadAudio(currentSong.audioURL);
+    scoreDisplay.textContent = `Frases Concluídas: ${score}`;
+  }
+
+
+// Function to select an option
+function selectOption(selectedOption, songIndex, phraseIndex) {
+  const currentSong = songsData[songIndex];
+  const currentAnswers = currentSong.answers[phraseIndex];
+  const optionBoxes = optionsContainer.querySelectorAll(".option-box");
+
+  optionBoxes.forEach((box) => {
+    box.classList.remove("correct", "wrong", "inactive");
+  });
+
+  if (currentAnswers.includes(selectedOption)) {
+    optionBoxes.forEach((box) => {
+      if (box.textContent === selectedOption) {
+        box.classList.add("correct");
       } else {
-        optionBoxes.forEach((box) => {
-          if (box.textContent === selectedOption) {
-            box.classList.add("wrong");
-          } else {
-            box.classList.add("inactive");
-          }
-        });
-        phraseContainer.textContent = "😕 Incorreto. Tente novamente.";
-        setTimeout(() => {
-          optionBoxes.forEach((box) => {
-            box.classList.remove("wrong");
-            box.classList.remove("inactive");
-          });
-          phraseContainer.textContent = songsData[songIndex].phrases[phraseIndex];
-        }, 1500);
+        box.classList.add("inactive");
       }
-    }
-  
-    function nextPhrase(songIndex, phraseIndex) {
-      if (songIndex === songsData.length - 1 && phraseIndex === songsData[songIndex].phrases.length - 1) {
-        phraseContainer.textContent = "Assine já para a versão completa! Vá p/ início e comece hoje!";
-        partLabelContainer.textContent = "";
-        optionsContainer.innerHTML = "";
-        scoreDisplay.textContent = `Frases Concluídas: ${score}`;
-        skipButton.style.display = "none";
-      } else if (phraseIndex < songsData[songIndex].phrases.length - 1) {
-        loadPhrase(songIndex, phraseIndex + 1);
-      } else if (songIndex < songsData.length - 1) {
-        currentAudio.pause(); // Pause the audio before moving to the next part
-        // Increase the current song index to skip to the next part
-        currentSongIndex++;
-        currentPhraseIndex = 0; // Reset the phrase index for the new part
-        score = 0; // Reset the score for the new part
-        // Load the first phrase of the new part
-        loadPhrase(currentSongIndex, currentPhraseIndex);
+    });
+    phraseContainer.textContent = "✔️ Correto!";
+    score++;
+    setTimeout(() => {
+      nextPhrase(songIndex, phraseIndex);
+    }, 1000);
+  } else {
+    optionBoxes.forEach((box) => {
+      if (box.textContent === selectedOption) {
+        box.classList.add("wrong");
+      } else {
+        box.classList.add("inactive");
       }
+    });
+    phraseContainer.textContent = "😕 Incorreto. Tente novamente.";
+    setTimeout(() => {
+      optionBoxes.forEach((box) => {
+        box.classList.remove("wrong");
+        box.classList.remove("inactive");
+      });
+      phraseContainer.textContent = songsData[songIndex].phrases[phraseIndex];
+    }, 1500);
+  }
+}
+
+
+
+  // Function to go to the next phrase or part
+  function nextPhrase(songIndex, phraseIndex) {
+    if (songIndex === songsData.length - 1 && phraseIndex === songsData[songIndex].phrases.length - 1) {
+      phraseContainer.textContent = "Assine já para a versão completa! Vá p/ início e comece hoje!";
+      partLabelContainer.textContent = "";
+      optionsContainer.innerHTML = "";
+      scoreDisplay.textContent = `Frases Concluídas: ${score}`;
+      skipButton.style.display = "none";
+    } else if (phraseIndex < songsData[songIndex].phrases.length - 1) {
+      loadPhrase(songIndex, phraseIndex + 1);
+    } else if (songIndex < songsData.length - 1) {
+      currentAudio.pause();
+      currentSongIndex++;
+      currentPhraseIndex = 0;
+      score = 0;
+      loadPhrase(currentSongIndex, currentPhraseIndex);
     }
+
+    // Reattach event listeners for audio control buttons
+    playButton.addEventListener("click", () => {
+      if (currentAudio) {
+        currentAudio.play();
+      }
+    });
+
+    pauseButton.addEventListener("click", () => {
+      if (currentAudio) {
+        currentAudio.pause();
+      }
+    });
+
+    restartAudioButton.addEventListener("click", () => {
+      if (currentAudio) {
+        currentAudio.currentTime = 0;
+        currentAudio.play();
+      }
+    });
+  }
+
+  // Add an event listener to the restart button
+  restartButton.addEventListener("click", () => {
+    currentPhraseIndex = 0;
+    score = 0;
+    loadPhrase(currentSongIndex, currentPhraseIndex);
+  });
+
+  // Add an event listener to the skip button
+  skipButton.addEventListener("click", () => {
+    if (currentSongIndex < songsData.length - 1) {
+      if (currentAudio) {
+        currentAudio.pause();
+      }
+      currentSongIndex++;
+      currentPhraseIndex = 0;
+      score = 0;
+      loadPhrase(currentSongIndex, currentPhraseIndex);
+    }
+  });
+
+  // Add an event listener to the go back button
+  goBackButton.addEventListener("click", () => {
+    if (currentSongIndex > 0) {
+      currentSongIndex--;
+      currentPhraseIndex = 0;
+      score = 0;
+      loadPhrase(currentSongIndex, currentPhraseIndex);
+    }
+  });
 
   // Initialize the first phrase and audio
   loadPhrase(currentSongIndex, currentPhraseIndex);
-  loadAudio(songsData[currentSongIndex].driveFileId);
+  loadAudio(songsData[currentSongIndex].audioURL);
 });
