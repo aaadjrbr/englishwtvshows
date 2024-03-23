7b205d278166217fa764e221dcc365494054a7b6
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2024-03-23 11:53:24 -0300

    test4

--- public/js/dictionary2.js
+++ public/js/dictionary2.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // Função para lidar com o envio do formulário de login
-    document.getElementById("loginForm").addEventListener("submit", function (e) {
+document.getElementById("loginForm").addEventListener("submit", function (e) {
     e.preventDefault();
     console.log("Formulário de login submetido"); // Adicione esta linha
 
