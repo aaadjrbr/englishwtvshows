4a8e0aef2eb588f1613c610a4fab1d59815feff7
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2024-03-28 01:11:12 -0300

    test5

--- test/reset-password.js
+++ test/reset-password.js
@@ -4,13 +4,13 @@ import { getAuth, confirmPasswordReset } from "https://www.gstatic.com/firebasej
 
 // Initialize Firebase using your existing configuration
 const firebaseConfig = {
-        apiKey: "AIzaSyBAGs-2m-qzleMxKS7-G4Vi53yOwhm99SA",
-        authDomain: "english-with-tv-shows.firebaseapp.com",
-        projectId: "english-with-tv-shows",
-        storageBucket: "english-with-tv-shows.appspot.com",
-        messagingSenderId: "382097760520",
-        appId: "1:382097760520:web:a8a33c9143a76e21320cbd",
-        measurementId: "G-M2SEMVXFPL"
+    apiKey: "AIzaSyBAGs-2m-qzleMxKS7-G4Vi53yOwhm99SA",
+    authDomain: "english-with-tv-shows.firebaseapp.com",
+    projectId: "english-with-tv-shows",
+    storageBucket: "english-with-tv-shows.appspot.com",
+    messagingSenderId: "382097760520",
+    appId: "1:382097760520:web:a8a33c9143a76e21320cbd",
+    measurementId: "G-M2SEMVXFPL"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,16 +22,21 @@ const oobCode = urlParams.get('oobCode');
 
 // Handle the password reset form submission
 document.getElementById('password-reset-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  const newPassword = document.getElementById('new-password').value;
-  confirmPasswordReset(auth, oobCode, newPassword)
-    .then(() => {
-      // Password reset successful
-      document.getElementById('message').innerText = 'Your password has been reset successfully.';
-      // Redirect the user to the login page or show a success message
-    })
-    .catch((error) => {
-      // Error occurred
-      document.getElementById('message').innerText = `Error: ${error.message}`;
-    });
+    event.preventDefault();
+    const newPassword = document.getElementById('new-password').value;
+    confirmPasswordReset(auth, oobCode, newPassword)
+        .then(() => {
+            // Password reset successful
+            document.getElementById('message').innerText = 'Your password has been reset successfully.';
+            // Redirect the user to the login page or show a success message
+            window.location.href = './test.html';
+        })
+        .catch((error) => {
+            // Error occurred
+            let errorMessage = error.message;
+            if (error.code === 'auth/invalid-action-code') {
+                errorMessage = 'Invalid or expired password reset link.';
+            }
+            document.getElementById('message').innerText = `Error: ${errorMessage}`;
+        });
 });
